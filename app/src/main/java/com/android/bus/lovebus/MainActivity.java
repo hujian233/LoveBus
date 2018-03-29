2a0ddcff30acf7823a69ab3e38dd943dcfde9fc1
haoran <421679009@qq.com>
2018-03-29 15:19:37 +0800

    Haoran commit test!

--- app/src/main/java/com/android/bus/lovebus/MainActivity.java
+++ app/src/main/java/com/android/bus/lovebus/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //this is a comment made by haoran
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
