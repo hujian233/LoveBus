0d64880f59310cf9061305e8468ccd8b24885793
hujiaojian <401387616@qq.com>
2018-03-29 15:43:55 +0800

    添加了实体类的getter和setter方法，属性包括用户id，用户名，密码，昵称，电话号码，管理员还是普通用户（role）后面有需要再添加

--- app/src/main/java/domain/User.java
+++ app/src/main/java/domain/User.java
@@ -13,7 +13,64 @@ public class User implements Serializable{
     private String username;
     private String password;
     private String nickname;
-    private String phonenumber;
+    private String phoneNumber;
     private Boolean role;
 
+    public User(int userId, String username, String password, String nickname, String phoneNumber, Boolean role) {
+        super();
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Boolean getRole() {
+        return role;
+    }
+
+    public void setRole(Boolean role) {
+        this.role = role;
+    }
 }
